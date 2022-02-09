be5b5cd374934988a2b04fdcb110a17a7c77d616
21TekSavvvy <jareddarochajj@gmail.com>
2022-02-09 10:47:03 -0800

    Add project files.

---
+++ Practice1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class AreaOfCircle
+{
+    static void Main()
+    {
+        // declare variables
+        double area =0;
+        string radius ;
+        // constant number of Pi
+        const double PI = Math.PI;
+
+        // bug
+        System.Console.Write("Enter radius value:  ");
+         radius=  Console.ReadLine();
+        double r = Convert.ToDouble(radius);
+        // Calculate area
+        area = PI * Math.Pow(r, 2);
+        // Print output
+        System.Console.WriteLine(area);
+
+        System.Console.ReadKey();
+    }
+}
